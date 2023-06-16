b850aa2096d56ed585cd084498f340319834519a
KevinTD15 <talaveradiazkevin1999@gmail.com>
2023-06-16 18:29:01 -0500

    deleted unused imports

--- satisfying.py
+++ satisfying.py
@@ -1,10 +1,6 @@
 from logic import logic
 
 def unit_clause(formula):
-    """
-    Takes a formula and returns a list of unit clauses. If none exist, then
-    return an empty list
-    """
     if formula is None:
         return []
 
@@ -30,48 +26,31 @@ def satisfying_converter(formula):
     return satisfying_assignment(form)
 
 def satisfying_assignment(formula):
-    """
-    Find a satisfying assignment for a given CNF formula.
-    Returns that assignment if one exists, or None otherwise.
-
-    >>> satisfying_assignment([])
     {}
-    >>> x = satisfying_assignment([[('a', True), ('b', False), ('c', True)]])
-    >>> x.get('a', None) is True or x.get('b', None) is False or x.get('c', None) is True
-    True
-    >>> satisfying_assignment([[('a', True)], [('a', False)]])
-    """
-    
-    
-    # base case #1
+
     if formula == []:
         return {}
 
-    # base case #2
     if [] in formula:
         return None
 
     soln = {}
     unit_clauses = unit_clause(formula)
-    # while loop only executes if bool evaluates to true
+
     while unit_clauses:
-        # this will update the formula
         for literal in unit_clauses:
             formula = help_update(formula, (literal[0], literal[1]))
             
-            #restate base cases for updated formula
             if [] in formula:
                 return None
 
             soln.setdefault(literal[0], literal[1])
             
-            #restated base case for updated formula
             if formula == []:
                 return soln
-        #recheck if there are any unit clauses in the updated formula
+
         unit_clauses = unit_clause(formula)
 
-    # the first statement of the formula
     literal = formula[0][0]
 
     if literal[1] is True:
@@ -79,7 +58,6 @@ def satisfying_assignment(formula):
     else:
         switch_bool = True
 
-    #checking both True and False scenarios
     nu_form = help_update(formula, literal)
     nu_form2 = help_update(formula, (literal[0], switch_bool))
 
@@ -96,29 +74,21 @@ def satisfying_assignment(formula):
     return None
 
 def help_update(formula, literal):
-    """
-    Given a formula, and a tuple of a variable and its boolean value,
-    return an updated formula that simplifies the initial formula, as well
-    as a dictionary which has values that are apart of the solution.
-    """
+
     new_formula = []
     for chain in formula:
         new_chain = []
         literal_found = False
         for statement in chain:
-            # if the literal is satisfied, then chain does not need to be considered
+
             if literal in chain:
                 literal_found = True
 
-            # if the literal is the opposite of the existing boolean, then the boolean
-            # value of the chain is dependent on every OTHER literal
             elif statement[0] == literal[0]:
                 continue
 
-            # if not related to the literal, then just add it into the new formula
             new_chain.append(statement)
 
-        # if the literal was not found in the formula, then the chain must be considered
         if not literal_found:
             new_formula.append(new_chain)
 
